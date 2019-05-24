d7414708b6c11410587e6bf019a9ba4abab58b93
Lucas Borges <devlucasborges@gmail.com>
2019-05-24 10:41:01 -0300

    Update CardNumber.php

--- src/CardNumber.php
+++ src/CardNumber.php
@@ -25,8 +25,7 @@ class CardNumber implements Rule
      * @return bool
      */
     public function passes($attribute, $value)
-    {
-        $value = preg_replace('/[^0-9]/', '', $value);    
+    {           
         try {
             return Factory::makeFromNumber($value)->isValidCardNumber();
         } catch (CreditCardLengthException $ex) {
